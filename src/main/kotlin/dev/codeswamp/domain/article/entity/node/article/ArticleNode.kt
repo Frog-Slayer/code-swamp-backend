75dc0c8f24af172bd85c0c7fead746d66f8d4d5a
frog-slayer <pj0642@gmail.com>
2025-04-30 23:33:12 +0900

    [feat] 그래프 조회를 위한 Neo4j 엔티티 생성 #4

---
+++ src/main/kotlin/dev/codeswamp/domain/article/entity/node/article/ArticleNode.kt
@@ -0,0 +1,13 @@
+package dev.codeswamp.domain.article.entity.node.article
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue
+import org.springframework.data.neo4j.core.schema.Id
+import org.springframework.data.neo4j.core.schema.Node
+
+@Node("Article")
+data class ArticleNode (
+    @Id @GeneratedValue val id: Long? = null,
+
+    val articleId: Long,//PK of RDB entity
+)
+
---
+++ src/main/kotlin/dev/codeswamp/domain/article/entity/node/folder/FolderNode.kt
@@ -0,0 +1,25 @@
+package dev.codeswamp.domain.article.entity.node.folder
+
+import dev.codeswamp.domain.article.entity.node.article.ArticleNode
+import jakarta.persistence.Embedded
+import org.springframework.data.neo4j.core.schema.GeneratedValue
+import org.springframework.data.neo4j.core.schema.Id
+import org.springframework.data.neo4j.core.schema.Node
+import org.springframework.data.neo4j.core.schema.Relationship
+
+@Node("Folder")
+data class FolderNode (
+    @Id @GeneratedValue
+    var id: Long? = null,
+
+    val folderId: Long,
+
+    @Relationship(value = "CONTAINS", direction = Relationship.Direction.OUTGOING)
+    val articles: List<ArticleNode> = mutableListOf(),
+
+    @Relationship(value = "HAS_CHILD", direction = Relationship.Direction.OUTGOING)
+    val children: List<FolderNode> = mutableListOf(),
+
+    @Relationship(value = "HAS_CHILD", direction = Relationship.Direction.INCOMING)
+    val parent: FolderNode? = null,
+)
