6f8cdb04c7fd3aa30691abb885c1107fc6da6545
frog-slayer <pj0642@gmail.com>
2025-05-03 10:23:16 +0900

    [feat] ArticleRepositoryImpl 필드 및 상속 관계 정의 #8

--- src/main/kotlin/dev/codeswamp/domain/article/infrastructure/persistence/repository/ArticleRepositoryImpl.kt
+++ src/main/kotlin/dev/codeswamp/domain/article/infrastructure/persistence/repository/ArticleRepositoryImpl.kt
@@ -1,4 +1,31 @@
 package dev.codeswamp.domain.article.infrastructure.persistence.repository
 
-class ArticleRepositoryImpl {
+import dev.codeswamp.domain.article.domain.model.Article
+import dev.codeswamp.domain.article.domain.repository.ArticleRepository
+import dev.codeswamp.domain.article.infrastructure.persistence.repository.rdb.ArticleContentJpaRepository
+import dev.codeswamp.domain.article.infrastructure.persistence.repository.rdb.ArticleMetadataJpaRepository
+
+class ArticleRepositoryImpl (
+    val articleMetadataJpaRepository: ArticleMetadataJpaRepository,
+    val articleContentJpaRepository: ArticleContentJpaRepository,
+) : ArticleRepository {
+    override fun save(article: Article): Article {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAllByIds(articleIds: List<Long>): List<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findById(articleId: Long): Article {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAllByKeywords(keywords: List<String>): List<Article> {
+        TODO("Not yet implemented")
+    }
 }
