ba559f8e70fcc5997ead88137ca365fafba091b5
frog-slayer <pj0642@gmail.com>
2025-05-03 10:17:16 +0900

    [feat] ArticleContentRepository, ArticleMetadataRepository 인터페이스 정의 #4

--- src/main/kotlin/dev/codeswamp/domain/article/infrastructure/persistence/repository/rdb/ArticleContentRepository.kt
+++ src/main/kotlin/dev/codeswamp/domain/article/infrastructure/persistence/repository/rdb/ArticleContentRepository.kt
@@ -2,6 +2,12 @@ package dev.codeswamp.domain.article.infrastructure.persistence.repository.rdb
 
 import dev.codeswamp.domain.article.infrastructure.persistence.entity.rdb.ArticleContentEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface ArticleContentRepository : JpaRepository<ArticleContentEntity, Long> {
+
+    override fun findById(id: Long): Optional<ArticleContentEntity?>
+    fun findAllByIdIsIn(ids: List<Long>): List<ArticleContentEntity>
+
+    fun save(articleContent: ArticleContentEntity)
 }
--- src/main/kotlin/dev/codeswamp/domain/article/infrastructure/persistence/repository/rdb/ArticleMetadataRepository.kt
+++ src/main/kotlin/dev/codeswamp/domain/article/infrastructure/persistence/repository/rdb/ArticleMetadataRepository.kt
@@ -2,6 +2,12 @@ package dev.codeswamp.domain.article.infrastructure.persistence.repository.rdb
 
 import dev.codeswamp.domain.article.infrastructure.persistence.entity.rdb.ArticleMetadataEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface ArticleMetadataRepository : JpaRepository<ArticleMetadataEntity, Long> {
+
+    override fun findById(id: Long): Optional<ArticleMetadataEntity?>
+    fun findByIdIsIn(ids: List<Long>): List<ArticleMetadataEntity>
+
+    fun save(articleMetadataEntity: ArticleMetadataEntity)
 }
