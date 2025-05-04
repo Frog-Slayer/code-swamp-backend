091300225587e3a6c937d3a8dfe333efdf48dc69
frog-slayer <pj0642@gmail.com>
2025-05-04 17:29:17 +0900

    [refactor] Fodler를 entity 패키지로 이동 #11

--- src/main/kotlin/dev/codeswamp/domain/folder/domain/entity/Folder.kt
+++ src/main/kotlin/dev/codeswamp/domain/folder/domain/entity/Folder.kt
@@ -1,14 +1,4 @@
-package dev.codeswamp.domain.folder.domain
-
-import dev.codeswamp.domain.article.domain.model.Article
-import dev.codeswamp.domain.user.entity.User
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
+package dev.codeswamp.domain.folder.domain.entity
 
 data class Folder(
     val id: Long? = null,
