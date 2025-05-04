7061c2b2f40b077f5b3d072ba5ea949dc640e413
frog-slayer <pj0642@gmail.com>
2025-05-04 21:53:13 +0900

    [feat] FolderService 구현 #11

--- src/main/kotlin/dev/codeswamp/domain/folder/domain/repository/folderRepository.kt
+++ src/main/kotlin/dev/codeswamp/domain/folder/domain/repository/folderRepository.kt
@@ -1,9 +1,10 @@
 package dev.codeswamp.domain.folder.domain.repository
 
 import dev.codeswamp.domain.folder.domain.entity.Folder
+import java.util.Optional
 
 interface folderRepository {
     fun save(folder: Folder): Folder
     fun delete(folder: Folder)
-    fun findById(folderId: Long): Folder
+    fun findById(folderId: Long): Optional<Folder>
 }
--- src/main/kotlin/dev/codeswamp/domain/folder/domain/service/FolderService.kt
+++ src/main/kotlin/dev/codeswamp/domain/folder/domain/service/FolderService.kt
@@ -1,25 +1,46 @@
 package dev.codeswamp.domain.folder.domain.service
 
 import dev.codeswamp.domain.folder.domain.entity.Folder
+import dev.codeswamp.domain.folder.domain.repository.folderRepository
 import org.springframework.stereotype.Service
 
 @Service
-class FolderService {
+class FolderService (
+    private val folderRepository: folderRepository
+){
 
-    fun create(ownerId: Long, name: String, parentId: Long?) : Folder {
-        TODO("Not yet implemented")
+    fun create(ownerId: Long, name: String, parentId: Long) : Folder {
+        val folder = Folder(
+            ownerId = ownerId,
+            name = name,
+            parentId = parentId
+        )
+        return folderRepository.save(folder)
     }
 
+    //TODO - Exception 구체화
     fun rename(folderId: Long, newName: String) {
-        TODO("Not yet implemented")
+        val folder = folderRepository.findById(folderId)
+            .orElseThrow{ IllegalArgumentException("Folder with id $folderId not found") }
+
+        folder.rename(newName)
+        folderRepository.save(folder)
     }
 
+    //TODO - Exception 구체화
     fun moveTo(folderId: Long, newParentId: Long) {
-        TODO("Not yet implemented")
+       val folder = folderRepository.findById(folderId)
+            .orElseThrow{ IllegalArgumentException("Folder with id $folderId not found") }
+
+        folder.moveTo(newParentId)
+        folderRepository.save(folder)
     }
 
     fun delete(folderId: Long) {
-        TODO("Not yet implemented")
+        val folder = folderRepository.findById(folderId)
+            .orElseThrow{ IllegalArgumentException("Folder with id $folderId not found") }
+
+        folderRepository.delete(folder)
     }
 
 
