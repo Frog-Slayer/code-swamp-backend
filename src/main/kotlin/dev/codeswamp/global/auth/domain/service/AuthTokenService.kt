a60171b6c2ee05822ae422beb84de445b930655f
frog-slayer <pj0642@gmail.com>
2025-05-05 01:10:15 +0900

    [remove] AuthTokenRepository 및 UserId 관련 기능 삭제 #14 - UserId를 통한 저장/삭제/조회 기능은 토큰을 Stateful하게 관리 - AuthToken에서 관련 기능을 제거하고 Stateless하게 유지

--- src/main/kotlin/dev/codeswamp/global/auth/domain/service/AuthTokenService.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/service/AuthTokenService.kt
@@ -7,8 +7,4 @@ interface AuthTokenService {
     fun generateToken(user: User): AuthToken
     fun validateToken(authToken: AuthToken): Boolean
     fun extractUserInfo(authToken: AuthToken): User
-
-    fun saveToken(userId: Long, authToken: AuthToken)
-    fun findByUserId(userId: Long): AuthToken?
-    fun deleteByUserId(userId: Long)
 }
