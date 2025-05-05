3c3736481c0739ac79e21b2768fb25b822ea8ba5
frog-slayer <pj0642@gmail.com>
2025-05-05 22:44:32 +0900

    [feat] RefreshToken에 revoked 필드를 추가 #14

--- src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/RefreshToken.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/RefreshToken.kt
@@ -5,6 +5,7 @@ import java.time.Instant
 data class RefreshToken (
     val value: String,
     val expiration: Instant,
+    val revoked: Boolean,
 ) {
     fun isExpired(): Boolean {
         return Instant.now().isAfter(expiration)
