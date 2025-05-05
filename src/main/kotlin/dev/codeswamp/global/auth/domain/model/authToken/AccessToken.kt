5ca6b717bc31e5c57af0456790f236c9af0360d5
frog-slayer <pj0642@gmail.com>
2025-05-06 07:58:05 +0900

    [feat] Token 객체들에 AuthUser 필드를 추가 #14

--- src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/AccessToken.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/AccessToken.kt
@@ -1,12 +1,10 @@
 package dev.codeswamp.global.auth.domain.model.authToken
 
+import dev.codeswamp.global.auth.domain.model.AuthUser
 import java.time.Instant
 
 data class AccessToken(
     val value: String,
+    val authUser: AuthUser,
     val expiration: Instant,
-) {
-    fun isExpired(): Boolean {
-        return  Instant.now().isAfter(expiration)
-    }
-}
+)
--- src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/RefreshToken.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/RefreshToken.kt
@@ -1,14 +1,12 @@
 package dev.codeswamp.global.auth.domain.model.authToken
 
+import dev.codeswamp.global.auth.domain.model.AuthUser
 import java.time.Instant
 
 data class RefreshToken (
     val value: String,
+    val authUser: AuthUser,
     val expiration: Instant,
     val revoked: Boolean,
-) {
-    fun isExpired(): Boolean {
-        return Instant.now().isAfter(expiration)
-    }
-}
+)
 
