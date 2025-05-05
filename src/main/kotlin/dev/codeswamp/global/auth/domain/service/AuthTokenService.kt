e899b60b5ed034d9577417561b6977bdf3a1ed4a
frog-slayer <pj0642@gmail.com>
2025-05-05 17:31:12 +0900

    [fix]: 도메인 서비스의 User를 AuthUser로 변경 #14

--- src/main/kotlin/dev/codeswamp/global/auth/domain/service/AuthTokenService.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/service/AuthTokenService.kt
@@ -1,10 +1,10 @@
 package dev.codeswamp.global.auth.domain.service
 
-import dev.codeswamp.domain.user.entity.User
 import dev.codeswamp.global.auth.domain.model.AuthToken
+import dev.codeswamp.global.auth.domain.model.AuthUser
 
 interface AuthTokenService {
-    fun generateToken(user: User): AuthToken
+    fun generateToken(authUser: AuthUser): AuthToken
     fun validateToken(authToken: AuthToken): Boolean
-    fun extractUserInfo(authToken: AuthToken): User
+    fun extractUserInfo(authToken: AuthToken): AuthUser
 }
