41dc2f80a3934cae3b9695f0856712c28029a980
frog-slayer <pj0642@gmail.com>
2025-05-07 03:52:12 +0900

    [feat] UserRepository 인터페이스도 raw String 대신 VO를 사용하도록 수정 #20

--- src/main/kotlin/dev/codeswamp/core/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/dev/codeswamp/core/user/domain/repository/UserRepository.kt
@@ -1,11 +1,13 @@
 package dev.codeswamp.core.user.domain.repository
 
+import dev.codeswamp.core.user.domain.model.Nickname
 import dev.codeswamp.core.user.domain.model.User
+import dev.codeswamp.core.user.domain.model.Username
 
 interface UserRepository {
     fun save(user: User): User
     fun findById(id: Long): User
-    fun findByUsername(username: String): User
+    fun findByUsername(username: Username): User
     fun findByEmail(email: String): User
-    fun findByNickname(nickname: String): User
+    fun findByNickname(nickname: Nickname): User
 }
