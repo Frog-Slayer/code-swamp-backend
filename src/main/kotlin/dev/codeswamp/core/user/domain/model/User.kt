df0706bedaa68943953faed3b3cf77c74375a085
frog-slayer <pj0642@gmail.com>
2025-05-07 03:42:44 +0900

    [feat] User 객체의 username, nickname 필드 타입을 변경 #20

--- src/main/kotlin/dev/codeswamp/core/user/domain/model/User.kt
+++ src/main/kotlin/dev/codeswamp/core/user/domain/model/User.kt
@@ -4,9 +4,9 @@ import javax.management.relation.Role
 
 data class User (
     val id: Long? = null,
-    val username: String,//변경 불가(사용자 ID)
+    val username: Username,//변경 불가(사용자 ID)
     val email: String,
-    var nickname: String,
+    var nickname: Nickname,
     var profileUrl: String? = null,
     val role: Role,
 ) {
