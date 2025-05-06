ef6b4f2d13dd33174f652df05b55828682920316
frog-slayer <pj0642@gmail.com>
2025-05-07 03:53:28 +0900

    [feat] UserRepository의 find...()메서드를 Optional<User>를 반환하도록 수정 #20

--- src/main/kotlin/dev/codeswamp/core/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/dev/codeswamp/core/user/domain/repository/UserRepository.kt
@@ -3,11 +3,12 @@ package dev.codeswamp.core.user.domain.repository
 import dev.codeswamp.core.user.domain.model.Nickname
 import dev.codeswamp.core.user.domain.model.User
 import dev.codeswamp.core.user.domain.model.Username
+import java.util.Optional
 
 interface UserRepository {
     fun save(user: User): User
-    fun findById(id: Long): User
-    fun findByUsername(username: Username): User
-    fun findByEmail(email: String): User
-    fun findByNickname(nickname: Nickname): User
+    fun findById(id: Long): Optional<User>
+    fun findByUsername(username: Username): Optional<User>
+    fun findByEmail(email: String): Optional<User>
+    fun findByNickname(nickname: Nickname): Optional<User>
 }
