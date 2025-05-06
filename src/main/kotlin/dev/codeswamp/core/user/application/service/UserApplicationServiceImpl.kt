0ff02058c42a088a3ac1c8b6e9b4a70ca025184f
frog-slayer <pj0642@gmail.com>
2025-05-07 06:17:37 +0900

    [feat] UserApplicationServiceImpl 구현 #20

---
+++ src/main/kotlin/dev/codeswamp/core/user/application/service/UserApplicationServiceImpl.kt
@@ -0,0 +1,47 @@
+package dev.codeswamp.core.user.application.service
+
+import dev.codeswamp.core.user.domain.model.Nickname
+import dev.codeswamp.core.user.domain.model.Role
+import dev.codeswamp.core.user.domain.model.User
+import dev.codeswamp.core.user.domain.model.Username
+import dev.codeswamp.core.user.domain.service.UserService
+import dev.codeswamp.core.user.presentation.dto.request.NicknameChangeRequestDto
+import dev.codeswamp.core.user.presentation.dto.request.UserSignUpRequestDto
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class UserApplicationServiceImpl(
+    private val userService: UserService,
+) : UserApplicationService {
+
+    @Transactional
+    override fun signUp(user: User, dto: UserSignUpRequestDto): User {//user -> detached
+        val signUpUser = User (
+                id = user.id,
+                email =  user.email,
+                username = Username.of(dto.username),
+                nickname = Nickname.of(dto.nickname),
+                profileUrl = dto.profileImageUrl,
+                role = Role.USER
+        )
+
+        userService.save(signUpUser)
+        return signUpUser
+    }
+
+    @Transactional
+    override fun modifyUserNickname(
+        user: User,//detached
+        nicknameDto: NicknameChangeRequestDto
+    ) {
+        user.nickname = Nickname.of(nicknameDto.nickname)
+        userService.save(user)//명시적 호출 필요
+    }
+
+    @Transactional
+    override fun modifyUserProfileImage(user: User, profileImageUrl: String?) {//user -> detached
+        user.profileUrl = profileImageUrl
+        userService.save(user)//명시적 호출 필요
+    }
+}
