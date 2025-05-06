37da7655ad7aaeaf37f0306b329d950845771697
frog-slayer <pj0642@gmail.com>
2025-05-06 09:22:18 +0900

    [feat] TokenService에서 구현하지 않고 각 서비스에 구현을 위임 #14

--- src/main/kotlin/dev/codeswamp/global/auth/domain/service/TokenService.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/service/TokenService.kt
@@ -6,32 +6,8 @@ import dev.codeswamp.global.auth.domain.model.authToken.RefreshToken
 import org.springframework.stereotype.Service
 
 @Service
-class TokenService : TokenIssueService, TokenStoreService, TokenValidationService {
-    override fun issueAccessToken(authUser: AuthUser): AccessToken {
-        TODO("Not yet implemented")
-    }
-
-    override fun issueRefreshToken(authUser: AuthUser): RefreshToken {
-        TODO("Not yet implemented")
-    }
-
-    override fun storeRefreshToken(token: RefreshToken) {
-        TODO("Not yet implemented")
-    }
-
-    override fun rotateRefreshToken(token: String, newToken: RefreshToken) {
-        TODO("Not yet implemented")
-    }
-
-    override fun findRefreshTokenByUserId(userId: Long): RefreshToken? {
-        TODO("Not yet implemented")
-    }
-
-    override fun validateAccessToken(accessToken: AccessToken) {
-        TODO("Not yet implemented")
-    }
-
-    override fun validateRefreshToken(refreshToken: RefreshToken) {
-        TODO("Not yet implemented")
-    }
-}
+class TokenService (
+    private val tokenIssueService: TokenIssueService,
+    private val tokenValidationService: TokenValidationService,
+    private val tokenStoreService: TokenStoreService,
+) : TokenIssueService by tokenIssueService, TokenValidationService by tokenValidationService , TokenStoreService by tokenStoreService
