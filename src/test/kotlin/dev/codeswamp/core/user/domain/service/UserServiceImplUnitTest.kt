8b0efaf5e085a2ad0e769407e68a90dd70a02f46
frog-slayer <pj0642@gmail.com>
2025-05-07 08:43:50 +0900

    [chore] AssetJ 의존성 추가 #20

--- src/test/kotlin/dev/codeswamp/core/user/domain/service/UserServiceImplUnitTest.kt
+++ src/test/kotlin/dev/codeswamp/core/user/domain/service/UserServiceImplUnitTest.kt
@@ -1,18 +1,23 @@
 package dev.codeswamp.core.user.domain.service
 
+import dev.codeswamp.core.user.domain.model.Nickname
+import dev.codeswamp.core.user.domain.model.Role
+import dev.codeswamp.core.user.domain.model.User
+import dev.codeswamp.core.user.domain.model.Username
 import dev.codeswamp.core.user.domain.repository.UserRepository
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
 @DisplayName("UserService 단위 테스트")
 class UserServiceImplUnitTest {
 
-    private lateinit var userService:  UserService;
+    private lateinit var userService: UserService;
     private lateinit var userRepository: UserRepository;
 
     @BeforeEach
     fun setUp() {
-
     }
-
 }
