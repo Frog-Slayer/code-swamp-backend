f2d783b02f4bf9e1ba7b3e750b763d8eb43401df
frog-slayer <pj0642@gmail.com>
2025-05-07 08:35:30 +0900

    [chore] 테스트를 위한 MockK 라이브러리 의존성 추가 #20

--- src/test/kotlin/dev/codeswamp/core/user/domain/service/UserServiceImplUnitTest.kt
+++ src/test/kotlin/dev/codeswamp/core/user/domain/service/UserServiceImplUnitTest.kt
@@ -1,10 +1,15 @@
 package dev.codeswamp.core.user.domain.service
 
+import dev.codeswamp.core.user.domain.repository.UserRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("UserService 단위 테스트")
-class UserServiceImplTest {
+class UserServiceImplUnitTest {
+
+    private lateinit var userService:  UserService;
+    private lateinit var userRepository: UserRepository;
+
     @BeforeEach
     fun setUp() {
 
