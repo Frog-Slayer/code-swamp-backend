498b3fc12179361268ffd43bdc6449829202d702
frog-slayer <pj0642@gmail.com>
2025-05-08 01:25:52 +0900

    [feat] JwtParser 구현 #14

--- src/main/kotlin/dev/codeswamp/global/auth/infrastructure/jwt/util/JwtParser.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/infrastructure/jwt/util/JwtParser.kt
@@ -3,15 +3,45 @@ package dev.codeswamp.global.auth.infrastructure.jwt.util
 import dev.codeswamp.global.auth.domain.model.authToken.RawAccessToken
 import dev.codeswamp.global.auth.domain.model.authToken.RawRefreshToken
 import dev.codeswamp.global.auth.domain.util.TokenParser
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import org.springframework.stereotype.Component
+import java.time.Instant
+import java.util.Base64
+import javax.crypto.SecretKey
 
 @Component
 class JwtParser : TokenParser {
+    private val jwtKey = "temporary_jwt_auth_key"
+    private val secretKey: SecretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtKey))
+
     override fun parseAccessToken(accessToken: String): RawAccessToken {
-        TODO("Not yet implemented")
+        val claims = Jwts.parser()
+            .verifyWith(secretKey)
+            .clockSkewSeconds(Long.MAX_VALUE)//ignore expiration here
+            .build()
+            .parseSignedClaims(accessToken)
+            .payload
+
+        return RawAccessToken(
+            value = accessToken,
+            sub = claims.subject,
+            expiration = claims.expiration.toInstant()
+        )
     }
 
     override fun parseRefreshToken(refreshToken: String): RawRefreshToken {
-        TODO("Not yet implemented")
+        val claims = Jwts.parser()
+            .verifyWith(secretKey)
+            .clockSkewSeconds(Long.MAX_VALUE)//ignore expiration here
+            .build()
+            .parseSignedClaims(refreshToken)
+            .payload
+
+        return RawRefreshToken(
+            value = refreshToken,
+            sub = claims.subject,
+            expiration = claims.expiration.toInstant()
+        )
     }
 }
