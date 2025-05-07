67ffcdb755653437762a95e6a15396c5ebd62f8b
frog-slayer <pj0642@gmail.com>
2025-05-08 03:17:09 +0900

    [feat] RedistTokenStorage 구현 #14

--- src/main/kotlin/dev/codeswamp/global/auth/infrastructure/redis/RedisTokenStorage.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/infrastructure/redis/RedisTokenStorage.kt
@@ -2,23 +2,44 @@ package dev.codeswamp.global.auth.infrastructure.redis
 
 import dev.codeswamp.global.auth.domain.model.authToken.ValidatedRefreshToken
 import dev.codeswamp.global.auth.domain.repository.TokenRepository
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
-class RedisTokenStorage : TokenRepository {
+class RedisTokenStorage(
+    private val redisTemplate: RedisTemplate<String, ValidatedRefreshToken>,
+) : TokenRepository {
+
     override fun storeRefreshToken(refreshToken: ValidatedRefreshToken) {
-        TODO("Not yet implemented")
+        val uid = refreshToken.authUser.id
+        val token = refreshToken.value
+
+        val uidKey = "token:user:$uid"
+        val tokenKey = "token:refresh:$token"
+
+        redisTemplate.opsForValue().set(uidKey, refreshToken)
+        redisTemplate.opsForValue().set(tokenKey, refreshToken)
     }
 
     override fun delete(refreshToken: ValidatedRefreshToken) {
-        TODO("Not yet implemented")
+        val uid = refreshToken.authUser.id
+        val token = refreshToken.value
+
+        val uidKey = "token:user:$uid"
+        val tokenKey = "token:refresh:$token"
+
+        redisTemplate.delete(uidKey)
+        redisTemplate.delete(tokenKey)
     }
 
     override fun findRefreshTokenByToken(token: String): ValidatedRefreshToken? {
-        TODO("Not yet implemented")
+        val tokenKey = "token:refresh:$token"
+
+        return redisTemplate.opsForValue().get(tokenKey)
     }
 
     override fun findRefreshTokenByUserId(userId: Long): ValidatedRefreshToken? {
-        TODO("Not yet implemented")
+        val uidKey = "token:user:$userId"
+        return redisTemplate.opsForValue().get(uidKey)
     }
 }
