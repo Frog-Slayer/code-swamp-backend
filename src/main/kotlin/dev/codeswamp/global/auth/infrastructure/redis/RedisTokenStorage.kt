cda06d74fb79f42851dd5d116d7c4f433721417d
frog-slayer <pj0642@gmail.com>
2025-05-08 03:32:19 +0900

    [feat] storeRefreshToken을 transactional하게 수정 #14

--- src/main/kotlin/dev/codeswamp/global/auth/infrastructure/redis/RedisTokenStorage.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/infrastructure/redis/RedisTokenStorage.kt
@@ -2,12 +2,15 @@ package dev.codeswamp.global.auth.infrastructure.redis
 
 import dev.codeswamp.global.auth.domain.model.authToken.ValidatedRefreshToken
 import dev.codeswamp.global.auth.domain.repository.TokenRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
+import java.util.concurrent.TimeUnit
 
 @Repository
 class RedisTokenStorage(
     private val redisTemplate: RedisTemplate<String, ValidatedRefreshToken>,
+    @Value("\${jwt.refresh-token-exp}") private val refreshTokenExpiration : Long
 ) : TokenRepository {
 
     override fun storeRefreshToken(refreshToken: ValidatedRefreshToken) {
@@ -17,8 +20,20 @@ class RedisTokenStorage(
         val uidKey = "token:user:$uid"
         val tokenKey = "token:refresh:$token"
 
-        redisTemplate.opsForValue().set(uidKey, refreshToken)
-        redisTemplate.opsForValue().set(tokenKey, refreshToken)
+        try {//transaction
+            redisTemplate.multi()
+
+            redisTemplate.opsForValue().set(uidKey, refreshToken)
+            redisTemplate.opsForValue().set(tokenKey, refreshToken)
+
+            redisTemplate.expire(uidKey,refreshTokenExpiration, TimeUnit.SECONDS    )
+            redisTemplate.expire(uidKey,refreshTokenExpiration, TimeUnit.SECONDS    )
+
+            redisTemplate.exec()
+        } catch (e: Exception) {
+            redisTemplate.discard()
+            throw e
+        }
     }
 
     override fun delete(refreshToken: ValidatedRefreshToken) {
