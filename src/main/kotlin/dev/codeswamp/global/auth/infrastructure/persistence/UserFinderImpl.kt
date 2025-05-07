48517e848af7555d38ca0c478a7c8d1d3cf668d5
frog-slayer <pj0642@gmail.com>
2025-05-08 00:44:19 +0900

    [feat] UserFinderImpl 구현 #14

--- src/main/kotlin/dev/codeswamp/global/auth/infrastructure/persistence/UserFinderImpl.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/infrastructure/persistence/UserFinderImpl.kt
@@ -1,16 +1,31 @@
 package dev.codeswamp.global.auth.infrastructure.persistence
 
+import dev.codeswamp.core.user.domain.repository.UserRepository
 import dev.codeswamp.global.auth.domain.model.AuthUser
 import dev.codeswamp.global.auth.domain.service.UserFinder
 import org.springframework.stereotype.Service
 
 @Service
-class UserFinderImpl : UserFinder {
+class UserFinderImpl(
+    private val userRepository: UserRepository //도메인 간 의존은 인프라를 통해 간접적으로
+) : UserFinder {
     override fun findBySubject(subject: String): AuthUser? {
-        TODO("Not yet implemented")
+        val user = userRepository.findByEmail(subject) ?: throw IllegalStateException("User not found")//TODO
+
+        return AuthUser(
+            id = user.id!!,
+            username = user.username.value!!,
+            roles = setOf(user.role.name),//TODO
+        )
     }
 
     override fun findById(id: Long): AuthUser? {
-        TODO("Not yet implemented")
+                val user = userRepository.findById(id) ?: throw IllegalStateException("User not found")//TODO
+
+        return AuthUser(
+            id = user.id!!,
+            username = user.username.value!!,
+            roles = setOf(user.role.name),//TODO
+        )
     }
 }
