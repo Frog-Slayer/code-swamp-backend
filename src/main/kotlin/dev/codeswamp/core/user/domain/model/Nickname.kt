925096c115ac694da3f2f5bbb036c35138e927da
frog-slayer <pj0642@gmail.com>
2025-05-07 09:40:41 +0900

    [fix] Nickname, Username 클래스에 equal(), hashCode() 오버라이드 #20 - 필드명을 value로 변경

--- src/main/kotlin/dev/codeswamp/core/user/domain/model/Nickname.kt
+++ src/main/kotlin/dev/codeswamp/core/user/domain/model/Nickname.kt
@@ -1,6 +1,6 @@
 package dev.codeswamp.core.user.domain.model
 
-class Nickname private constructor(val nickname: String) {
+class Nickname private constructor(val value: String) {
     companion object {
         fun of(nickname: String): Nickname {
             require(isValidNickname(nickname)) {
@@ -13,5 +13,13 @@ class Nickname private constructor(val nickname: String) {
         private fun isValidNickname(nickname: String): Boolean {
             return nickname.length in 2..12 && nickname.matches("^[a-zA-Z0-9가-힣]*$".toRegex())
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is Nickname && other.value == this.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
     }
 }
