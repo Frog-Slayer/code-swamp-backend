2d8a9569d77295e86a2f23eb73489fed93a5ef50
frog-slayer <pj0642@gmail.com>
2025-05-07 22:43:23 +0900

    [refactor] JWT의 issuer, validator, store를 삭제 #14 - JWT는 파싱만을 구현하도록 수정

--- src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/RawAccessToken.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/model/authToken/RawAccessToken.kt
@@ -1,6 +1,5 @@
 package dev.codeswamp.global.auth.domain.model.authToken
 
-import dev.codeswamp.global.auth.domain.model.AuthUser
 import java.time.Instant
 
 data class RawAccessToken(
---
+++ src/main/kotlin/dev/codeswamp/global/auth/domain/service/TokenServiceImpl.kt
@@ -0,0 +1,47 @@
+package dev.codeswamp.global.auth.domain.service
+
+import dev.codeswamp.global.auth.domain.model.AuthUser
+import dev.codeswamp.global.auth.domain.model.authToken.RawAccessToken
+import dev.codeswamp.global.auth.domain.model.authToken.ValidatedAccessToken
+import dev.codeswamp.global.auth.domain.model.authToken.RawRefreshToken
+import dev.codeswamp.global.auth.domain.model.authToken.ValidatedRefreshToken
+import org.springframework.stereotype.Service
+
+@Service
+class TokenServiceImpl : TokenIssueService, TokenValidationService, TokenStoreService {
+
+    fun refreshAccessToken(refreshToken: ValidatedRefreshToken): ValidatedAccessToken {
+        TODO("not yet implemented")
+    }
+
+    override fun issueAccessToken(authUser: AuthUser): ValidatedAccessToken {
+        TODO("Not yet implemented")
+    }
+
+    override fun issueRefreshToken(authUser: AuthUser): ValidatedRefreshToken {
+        TODO("Not yet implemented")
+    }
+
+    override fun validateAccessToken(accessToken: RawAccessToken): ValidatedAccessToken {
+        TODO("Not yet implemented")
+    }
+
+    override fun validateRefreshToken(refreshToken: RawRefreshToken): ValidatedRefreshToken {
+        TODO("Not yet implemented")
+    }
+
+    override fun storeRefreshToken(token: ValidatedRefreshToken) {
+        TODO("Not yet implemented")
+    }
+
+    override fun rotateRefreshToken(
+        token: String,
+        newToken: ValidatedRefreshToken
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun findRefreshTokenByUserId(userId: Long): ValidatedRefreshToken? {
+        TODO("Not yet implemented")
+    }
+}
