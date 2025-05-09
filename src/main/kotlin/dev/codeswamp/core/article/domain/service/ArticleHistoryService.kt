481ceef76c11a0bbe29e4a7e0966dbeed41e3c93
frog-slayer <pj0642@gmail.com>
2025-05-10 02:51:33 +0900

    [feat] ArticleHistoryService, ArticleService 도메인 서비스 작성 #25

--- src/main/kotlin/dev/codeswamp/core/article/domain/service/ArticleHistoryService.kt
+++ src/main/kotlin/dev/codeswamp/core/article/domain/service/ArticleHistoryService.kt
@@ -1,4 +1,24 @@
 package dev.codeswamp.core.article.domain.service
 
+import dev.codeswamp.core.article.domain.model.Article
+import dev.codeswamp.core.article.domain.model.ArticleDiff
+import org.springframework.stereotype.Service
+
+@Service
 class ArticleHistoryService {
+    fun saveDiff(original: Article?, updated: Article) : ArticleDiff? {
+        TODO("Not yet implemented")
+    }
+
+    fun getHistory(articleId: Long) : List<ArticleDiff> {
+        TODO("Not yet implemented")
+    }
+
+    fun buildFullContentFromHistory(history: List<ArticleDiff>) : String {
+        TODO("Not yet implemented")
+    }
+
+    fun rollbackTo(articleId: Long, versionId: Long): Article {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/dev/codeswamp/core/article/domain/service/ArticleService.kt
+++ src/main/kotlin/dev/codeswamp/core/article/domain/service/ArticleService.kt
@@ -2,31 +2,44 @@ package dev.codeswamp.core.article.domain.service
 
 import dev.codeswamp.core.article.domain.model.Article
 import dev.codeswamp.core.article.domain.repository.ArticleRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class ArticleService (
-    private val articleRepository: ArticleRepository
+    private val articleRepository: ArticleRepository,
+    private val articleHistoryService: ArticleHistoryService
 ){
 
-    fun save(article: Article): Article {
-        return articleRepository.save(article)
+    @Transactional
+    fun create(article: Article): Article {
+        val saved = articleRepository.save(article)
+        articleHistoryService.saveDiff(null, saved)
+        return saved
     }
 
-    fun delete(article: Article) {
-        articleRepository.delete(article)
-    }
 
-    fun findAllByIds(articleIds : List<Long>): List<Article> {
-        return articleRepository.findAllByIds(articleIds)
+    @Transactional
+    fun update(article: Article): Article {
+        val original = articleRepository.findById(article.id!!) ?: throw IllegalArgumentException("Article does not exist")
+        val updated = articleRepository.save(article)
+
+        articleHistoryService.saveDiff(original, updated)
+        return updated
     }
 
     fun findById(articleId: Long): Article? {
         return articleRepository.findById(articleId)
     }
 
-    fun publish(article: Article) {
-        article.publish()
-        articleRepository.save(article)
+    fun findAllByIds(articleIds: List<Long>): List<Article> {
+        return articleRepository.findAllByIds(articleIds)
+    }
+
+    @Transactional
+    fun deleteById(articleId: Long) {
+        val article = articleRepository.findById(articleId) ?: throw IllegalArgumentException("Article not found")
+
+        articleRepository.delete(article)
     }
 }
