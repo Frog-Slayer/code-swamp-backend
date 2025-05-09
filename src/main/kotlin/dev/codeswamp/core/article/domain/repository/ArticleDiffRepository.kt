a07da37bd1e9a5a0acf61e91e8f1024544ac62e9
frog-slayer <pj0642@gmail.com>
2025-05-10 03:53:46 +0900

    [feat] ArticleDiffRepository 인터페이스 작성 #25

---
+++ src/main/kotlin/dev/codeswamp/core/article/domain/repository/ArticleDiffRepository.kt
@@ -0,0 +1,10 @@
+package dev.codeswamp.core.article.domain.repository
+
+import dev.codeswamp.core.article.domain.model.ArticleDiff
+
+interface ArticleDiffRepository {
+    fun save(articleDiff: ArticleDiff): ArticleDiff
+    fun findById(id: Long) : ArticleDiff?
+    fun findByArticleId(articleId: Long) : List<ArticleDiff>
+    fun deleteByArticleId(articleId: Long)
+}
