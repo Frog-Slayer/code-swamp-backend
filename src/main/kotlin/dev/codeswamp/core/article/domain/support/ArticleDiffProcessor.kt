e5145a644267c1184d17b6ac84afd10a8ef33b39
frog-slayer <pj0642@gmail.com>
2025-05-10 04:54:57 +0900

    [feat] ArticleDiffProcessor 인터페이스에 메서드를 추가 #25

--- src/main/kotlin/dev/codeswamp/core/article/domain/support/ArticleDiffProcessor.kt
+++ src/main/kotlin/dev/codeswamp/core/article/domain/support/ArticleDiffProcessor.kt
@@ -3,4 +3,6 @@ package dev.codeswamp.core.article.domain.support
 interface ArticleDiffProcessor {
     fun calculateDiff(old: String, new: String) : String?
     fun buildFullContentFromHistory(history: List<String>) : String
+    fun findLCA(versionAId: Long, versionBId: Long): Long
+    fun findNearestSnapShotBefore(versionId: Long): Long
 }
