6ba9213c40ffcdc25f1b2c5ae3dc62c80d8075e1
frog-slayer <pj0642@gmail.com>
2025-05-10 07:53:00 +0900

    [feat] Neo4j 그래프 탐색을 위한 ArticleNode, HistoryNode를 정의 #25

--- src/main/kotlin/dev/codeswamp/core/article/infrastructure/graph/node/HistoryNode.kt
+++ src/main/kotlin/dev/codeswamp/core/article/infrastructure/graph/node/HistoryNode.kt
@@ -1,16 +1,19 @@
 package dev.codeswamp.core.article.infrastructure.graph.node
 
+import dev.codeswamp.core.article.domain.model.Article
 import org.springframework.data.neo4j.core.schema.GeneratedValue
 import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
 import org.springframework.data.neo4j.core.schema.Relationship
 
-@Node("Article")
-data class ArticleNode (
+
+@Node("History")
+data class HistoryNode(
     @Id @GeneratedValue val id: Long? = null,
 
-    val articleId: Long,//PK of RDB entity
+    @Relationship(type = "NEXT", direction = Relationship.Direction.INCOMING)
+    var previous: HistoryNode? = null,
 
-    @Relationship(type = "HAS_VERSION", direction = Relationship.Direction.OUTGOING)
-    var currentVersion: HistoryNode? = null
+    @Relationship(type = "NEXT", direction = Relationship.Direction.OUTGOING)
+    var next: HistoryNode? = null
 )
