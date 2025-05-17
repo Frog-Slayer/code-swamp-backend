86ebaf6dac4bc0888589b11913ec8b4b20b13d03
frog-slayer <pj0642@gmail.com>
2025-05-17 16:07:32 +0900

    [feat] SecurityConfig 작성 #30 - UserFinderImpl에서 예외 삭제

--- src/main/kotlin/dev/codeswamp/global/auth/infrastructure/persistence/UserFinderImpl.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/infrastructure/persistence/UserFinderImpl.kt
@@ -10,32 +10,38 @@ class UserFinderImpl(
     private val userRepository: UserRepository //도메인 간 의존은 인프라를 통해 간접적으로
 ) : UserFinder {
     override fun findBySubject(subject: String): AuthUser? {
-        val user = userRepository.findByEmail(subject) ?: throw IllegalStateException("User not found")//TODO
+        val user = userRepository.findByEmail(subject)
 
-        return AuthUser(
-            id = user.id!!,
-            username = user.email,
-            roles = setOf(user.role.name),//TODO
-        )
+        return user?.let {
+            AuthUser(
+                id = it.id!!,
+                username = it.email,
+                roles = setOf(it.role.name),//TODO
+            )
+        }
     }
 
     override fun findById(id: Long): AuthUser? {
-        val user = userRepository.findById(id) ?: throw IllegalStateException("User not found")//TODO
+        val user = userRepository.findById(id)
 
-        return AuthUser(
-            id = user.id!!,
-            username = user.email,
-            roles = setOf(user.role.name),//TODO
-        )
+        return user?.let {
+            AuthUser(
+                id = it.id!!,
+                username = it.email,
+                roles = setOf(it.role.name),//TODO
+            )
+        }
     }
 
     override fun findByEmail(email: String): AuthUser? {
         val user = userRepository.findByEmail(email)
 
-        return AuthUser(
-            id = user?.id!!,
-            username = user.email,
-            roles = setOf(user.role.name),//TODO
-        )
+        return user?.let {
+            AuthUser(
+                id = it.id!!,
+                username = it.email,
+                roles = setOf(it.role.name),//TODO
+            )
+        }
     }
 }
