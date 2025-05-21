c96174411d7de611ea6a29ee20426d71789624e6
frog-slayer <pj0642@gmail.com>
2025-05-21 18:17:10 +0900

    [fix] userController.signup() 파라미터에 @RequestBody가 빠져있던 것을 수정 #30

--- src/main/kotlin/dev/codeswamp/core/user/presentation/controller/UserController.kt
+++ src/main/kotlin/dev/codeswamp/core/user/presentation/controller/UserController.kt
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,7 +23,7 @@ class UserController(
 ) {
 
     @PostMapping("/signup")
-    fun signup(signUpRequestDto: SignUpRequestDto) {
+    fun signup(@RequestBody signUpRequestDto: SignUpRequestDto) {
         val userId = authServiceAcl.verifyTokenAndCreateAuthUser(signUpRequestDto.token, signUpRequestDto.email)
 
         userApplicationService.signUp(SignUpCommand(
