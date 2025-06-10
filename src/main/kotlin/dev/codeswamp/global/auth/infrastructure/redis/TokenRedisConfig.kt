e2b25cfa7c401350242bdd3e62dab0a2e98339c7
frog-slayer <pj0642@gmail.com>
2025-06-11 08:27:07 +0900

    [feat] RedisConnectionFactory를 전역으로 분리 #16

--- src/main/kotlin/dev/codeswamp/global/auth/infrastructure/redis/TokenRedisConfig.kt
+++ src/main/kotlin/dev/codeswamp/global/auth/infrastructure/redis/TokenRedisConfig.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import dev.codeswamp.global.auth.domain.model.token.ValidatedRefreshToken
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -13,23 +14,14 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-class TokenRedisConfig {
-
-    @Bean
-    fun tokenRedisConnectionFactory() : LettuceConnectionFactory {
-        val redisConfig = RedisStandaloneConfiguration().apply {
-            hostName= "localhost"
-            port = 6379
-            database = 0
-        }
-
-        return LettuceConnectionFactory(redisConfig)
-    }
+class TokenRedisConfig (
+    private val redisConnectionFactory: RedisConnectionFactory,
+) {
 
     @Bean
     fun refreshTokenTemplate(): RedisTemplate<String, ValidatedRefreshToken> {
         val template = RedisTemplate<String, ValidatedRefreshToken>()
-        template.connectionFactory = tokenRedisConnectionFactory()
+        template.connectionFactory = redisConnectionFactory
         template.keySerializer = StringRedisSerializer()
 
         val objectMapper = ObjectMapper().apply {
@@ -45,7 +37,7 @@ class TokenRedisConfig {
     @Bean(name = ["temporaryTokenTemplate"])
     fun temporaryTokenTemplate(): RedisTemplate<String, String> {
         val template = RedisTemplate<String, String>()
-        template.connectionFactory = tokenRedisConnectionFactory()
+        template.connectionFactory = redisConnectionFactory
 
         template.keySerializer = StringRedisSerializer()
         template.valueSerializer = StringRedisSerializer()
