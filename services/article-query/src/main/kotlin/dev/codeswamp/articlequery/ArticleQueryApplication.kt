f1f2f881a32c329194b2523406cd33a5a139d526
frog-slayer <pj0642@gmail.com>
2025-07-11 14:48:41 +0900

    [feat] redis의 조회수 flush를 위한 ViewCountFlusher 구현 #74

--- services/article-query/src/main/kotlin/dev/codeswamp/articlequery/ArticleQueryApplication.kt
+++ services/article-query/src/main/kotlin/dev/codeswamp/articlequery/ArticleQueryApplication.kt
@@ -1,9 +1,12 @@
 package dev.codeswamp.articlequery
 
+import dev.codeswamp.framework.infrastructure.config.FrameworkBeansConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.context.annotation.Import
 
+@Import(FrameworkBeansConfig::class)
 @SpringBootApplication
 class ArticleQueryApplication
 
---
+++ services/article-query/src/main/kotlin/dev/codeswamp/articlequery/application/viewcount/service/ViewCountFlusherImpl.kt
@@ -0,0 +1,53 @@
+package dev.codeswamp.articlequery.application.viewcount.service
+
+import dev.codeswamp.articlequery.application.viewcount.ViewCountFlushedEvent
+import dev.codeswamp.articlequery.application.viewcount.ViewCountRepository
+import dev.codeswamp.core.application.event.eventbus.EventPublisher
+import dev.codeswamp.core.infrastructure.persistence.TransactionExecutor
+import dev.codeswamp.framework.application.outbox.EventRecorder
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import java.time.Instant
+
+@Service
+class ViewCountFlusherImpl(
+    @Qualifier("viewCountRedisTemplate") private val redis: ReactiveRedisOperations<String, String>,
+    private val viewCountRepository: ViewCountRepository,
+    private val eventRecorder: EventRecorder,
+    private val transactionExecutor: TransactionExecutor
+): ViewCountFlusher {
+    private val prefix = "view:article:"
+
+    @Scheduled(fixedRate = 600000)
+    override suspend fun flush() {
+        val keys = redis.keys("$prefix:*").collectList().awaitSingle()
+
+        val viewCounts = keys.mapNotNull { key ->
+            val articleId = key.removePrefix(prefix).toLongOrNull() ?: return@mapNotNull null
+            val count = redis.opsForZSet().size(key).awaitFirstOrNull() ?: 0
+            Pair(articleId, count.toInt())
+        }.filter { it.second > 0 }
+
+        val batches = viewCounts.chunked(100)
+
+        transactionExecutor.execute {
+            batches.forEach { chunk ->
+                eventRecorder.record(ViewCountFlushedEvent(
+                    chunk
+                ))
+            }
+        }
+
+        keys.forEach { key ->
+            val articleId = key.removePrefix(prefix)
+                .toLongOrNull()
+                ?:  return@forEach
+
+            viewCountRepository.clearViewers(articleId)
+        }
+    }
+}
